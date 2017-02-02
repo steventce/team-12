87c3adc25343370917e083e1189cd7e665e37ed4
Evan Fleming <evan.gordon.fleming@gmail.com>
2017-02-01 22:03:59 -0800

    Evan: create script initial draft

---
+++ DB_Scripts/CreateDB.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE Resource_Booker;
+
+USE Resource_Booker;
+
+CREATE TABLE Locations(
+	LocationId INT PRIMARY KEY,
+	BuildingName VARCHAR(20),
+	StreetName VARCHAR(20),
+	City VARCHAR(20),
+	ProvinceState VARCHAR(20),
+	PostalCode VARCHAR(20)
+);
+
+CREATE TABLE ResourceTypes(
+	ResourceType VARCHAR(20) PRIMARY KEY
+);
+
+CREATE TABLE Resources(
+	ResourceId INT PRIMARY KEY,
+	LocationId INT,
+	ResourceType VARCHAR(20),
+	FOREIGN KEY (LocationId) REFERENCES Locations(LocationId),
+	FOREIGN KEY (ResourceType) REFERENCES ResourceTypes(ResourceType)
+);
+
+CREATE TABLE Desks(
+	ResourceId INT PRIMARY KEY,
+	Floor VARCHAR(20),
+	Section VARCHAR(20),
+	DeskNumber VARCHAR(20),
+	FOREIGN KEY (ResourceId) REFERENCES Resources(ResourceId)
+);
+
+CREATE TABLE Reservations (
+	ReservationId INT PRIMARY KEY, 
+	ResourceId INT, 
+	StaffId VARCHAR(20), 
+	StaffName VARCHAR(20), 
+	StaffDepartment VARCHAR(20),
+	StaffEmail VARCHAR(20),
+	StartDate DATETIME,
+	EndDate DATETIME,
+	FOREIGN KEY (ResourceId) REFERENCES Resources(ResourceId)
+);
+
