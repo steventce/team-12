3e40ed071a34bef6dcd449ed89532136851d10b1
VaniaBudiman <evaniab@msn.com>
2017-02-11 16:24:47 -0800

    Added locations table with default Vancouver office entry

--- client/src/containers/Admin/Locations/Locations.js
+++ client/src/containers/Admin/Locations/Locations.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 class Locations extends Component {
   render() {
+    var locations =
+    [{
+      locationName: 'Broadway Building Green',
+      locationAddress: '2910 Virtual Way, Vancouver, BC, V5M 0B2',
+      editLocation: 'Edit',
+      deleteLocation: 'x'
+    }];
     return (
       <div>
-        <h1>Locations Entry</h1>
+        <h2>Locations</h2>
+        <div className='container tableContainer'>
+          <BootstrapTable data={locations} striped={true} hover={true}>
+              <TableHeaderColumn dataField='locationName' isKey={true} dataAlign='center' dataSort={true}>Name</TableHeaderColumn>
+              <TableHeaderColumn dataField='locationAddress' dataAlign='center' dataSort={true}>Address</TableHeaderColumn>
+              <TableHeaderColumn dataField='editLocation' dataAlign='center' width='150'></TableHeaderColumn>
+              <TableHeaderColumn dataField='deleteLocation' dataAlign='center' width='100'>Delete?</TableHeaderColumn>
+          </BootstrapTable>
+        </div>
       </div>
     );
   }
