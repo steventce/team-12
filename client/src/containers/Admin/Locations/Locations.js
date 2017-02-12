7447fe5cf948d0ff872cedf2503b6ffea466e10b
VaniaBudiman <evaniab@msn.com>
2017-02-11 16:58:10 -0800

    Added button that will be able to create new locations

--- client/src/containers/Admin/Locations/Locations.js
+++ client/src/containers/Admin/Locations/Locations.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { div, Grid, Row, Col, ButtonToolbar, Button } from 'react-bootstrap';
+import './Locations.css';
 
 class Locations extends Component {
   render() {
+
     var locations =
     [{
       locationName: 'Broadway Building Green',
@@ -10,17 +13,30 @@ class Locations extends Component {
       editLocation: 'Edit',
       deleteLocation: 'x'
     }];
+
     return (
       <div>
         <h2>Locations</h2>
+
         <div className='container tableContainer'>
           <BootstrapTable data={locations} striped={true} hover={true}>
               <TableHeaderColumn dataField='locationName' isKey={true} dataAlign='center' dataSort={true}>Name</TableHeaderColumn>
               <TableHeaderColumn dataField='locationAddress' dataAlign='center' dataSort={true}>Address</TableHeaderColumn>
-              <TableHeaderColumn dataField='editLocation' dataAlign='center' width='150'></TableHeaderColumn>
-              <TableHeaderColumn dataField='deleteLocation' dataAlign='center' width='100'>Delete?</TableHeaderColumn>
+              <TableHeaderColumn dataField='editLocation' dataAlign='center' width='150px'></TableHeaderColumn>
+              <TableHeaderColumn dataField='deleteLocation' dataAlign='center' width='100px'>Delete?</TableHeaderColumn>
           </BootstrapTable>
         </div>
+
+        <Grid>
+          <Row className="show-grid">
+            <Col md={2}>
+              <ButtonToolbar className='addLocationBtnContainer'>
+                <Button>Add New Location</Button>
+              </ButtonToolbar>
+            </Col>
+          </Row>
+         </Grid>
+
       </div>
     );
   }
